e2d0e6a449ee3f5c93a6fbc2a061de19a137607d
Last_Occultism <tobiasbakker2007@gmail.com>
2025-11-25 13:49:37 +0100

    SOMEHOW uploaded the wrong version. This one should include the Dissapearing doors
    V0.2

---
+++ Assets/Scripts/OpenCloseGate.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class OpenCloseGate : MonoBehaviour
+{
+    private bool isVisible = true;
+    void Start()
+    {
+        gameObject.SetActive(true);
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            isVisible = !isVisible;
+            gameObject.SetActive(isVisible);
+        }
+    }
+}
